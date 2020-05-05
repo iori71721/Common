d0cfd6424086b316d41bd365ca8f541e6c6ce896
iori <iori71721@gmail.com>
2020-05-05 14:03:00 +0800

    version 1.0.3 1. 新增水平跑馬燈計算器 2. 圓形圖案，點擊顯示點擊框元件

---
+++ common/src/main/java/com/iori/custom/common/marquee/HorizontalMarquee.java
@@ -0,0 +1,140 @@
+package com.iori.custom.common.marquee;
+
+import androidx.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * useage:
+ * 1.create object
+ * 2.calc marquee width and setup
+ * 3.setup moveStep
+ * 4.call {@link #start()}
+ * 5.cycle call {@link #move()} to implements marquee
+ */
+public class HorizontalMarquee {
+    public static final int MOVE_LEFT_FROM_RIGHT =0;
+    public static final int MOVE_RIGHT_FROM_LEFT =1;
+
+    private final int inValidPosition=Integer.MIN_VALUE;
+    public int startPosition;
+    public int endPosition;
+    private int beforePosition=inValidPosition;
+    private int currentPosition=inValidPosition;
+    private final @MoveType int moveType;
+    public int moveStep;
+    public int marqueeWidth=0;
+    private final MoveBehavior moveBehavior;
+    private boolean stop;
+
+    public HorizontalMarquee(@MoveType int moveType, int startPosition, int endPosition, int marqueeWidth, MoveBehavior moveBehavior) {
+        this.moveType = moveType;
+        this.startPosition=startPosition;
+        this.endPosition=endPosition;
+        this.marqueeWidth=marqueeWidth;
+        this.moveBehavior=moveBehavior;
+    }
+
+    private void setupMvoeStartPosition(){
+        if(currentPosition == inValidPosition){
+            currentPosition=calcInitPosition(moveType,marqueeWidth);
+            beforePosition=currentPosition;
+        }
+    }
+
+    private int calcInitPosition(@MoveType int moveType,int marqueeWidth){
+        int initPosition=0;
+        switch (moveType){
+            case MOVE_RIGHT_FROM_LEFT:
+                initPosition=startPosition-marqueeWidth;
+                break;
+            case MOVE_LEFT_FROM_RIGHT:
+                initPosition=endPosition;
+                break;
+        }
+        return initPosition;
+    }
+
+    private int calcEndPosition(@MoveType int moveType,int marqueeWidth){
+        int endPosition=0;
+        switch (moveType){
+            case MOVE_RIGHT_FROM_LEFT:
+                endPosition=this.endPosition;
+                break;
+            case MOVE_LEFT_FROM_RIGHT:
+                endPosition=startPosition-marqueeWidth;
+                break;
+        }
+        return endPosition;
+    }
+
+    private void fixedCurrentPosition(@MoveType int moveType,int marqueeWidth){
+        int endPosition=calcEndPosition(moveType,marqueeWidth);
+        switch (moveType){
+            case MOVE_RIGHT_FROM_LEFT:
+                if(currentPosition > endPosition){
+                    currentPosition=endPosition;
+                }
+                break;
+            case MOVE_LEFT_FROM_RIGHT:
+                if(currentPosition < endPosition){
+                    currentPosition=endPosition;
+                }
+                break;
+        }
+    }
+
+    private void prepareMove(@MoveType int moveType,int marqueeWidth){
+        int endPosition=calcEndPosition(moveType, marqueeWidth);
+        beforePosition=currentPosition;
+        if(currentPosition == endPosition){
+            currentPosition=calcInitPosition(moveType,marqueeWidth);
+        }
+    }
+
+    private void move(@MoveType int moveType,int moveStep){
+        switch (moveType){
+            case MOVE_RIGHT_FROM_LEFT:
+                currentPosition+=moveStep;
+                break;
+            case MOVE_LEFT_FROM_RIGHT:
+                currentPosition-=moveStep;
+                break;
+        }
+    }
+
+    public void move(){
+        setupMvoeStartPosition();
+        prepareMove(moveType,marqueeWidth);
+        move(moveType,moveStep);
+        fixedCurrentPosition(moveType,marqueeWidth);
+        if(moveBehavior != null){
+            if(!stop) {
+                moveBehavior.move(beforePosition, currentPosition);
+            }
+        }
+    }
+
+    public void start(){
+        stop=false;
+    }
+
+    public void stop(){
+        stop=true;
+        currentPosition=inValidPosition;
+        beforePosition=inValidPosition;
+    }
+
+    public boolean isStop() {
+        return stop;
+    }
+
+    public static interface MoveBehavior{
+        void move(int beforePosition,int currentPosition);
+    }
+
+    @IntDef({MOVE_LEFT_FROM_RIGHT, MOVE_RIGHT_FROM_LEFT})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface MoveType{}
+}
---
+++ common/src/main/java/com/iori/custom/common/view/CycleColorView.java
@@ -0,0 +1,139 @@
+package com.iori.custom.common.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.iori.custom.common.R;
+
+/**
+ * useage:
+ * 1.setup {@link #cycleColor},{@link #frameColor}
+ * 2.setup {@link #framePaddingPx},{@link #frameWidthPx}
+ */
+public class CycleColorView extends View {
+    private final int defaultSizePx =100;
+    private final int defaultColor= Color.BLACK;
+    private final int drawOffsetPx=1;
+    private int cycleColor =defaultColor;
+    private int frameColor=defaultColor;
+    private int framePaddingPx;
+    private int frameWidthPx;
+    private boolean choose;
+
+    public CycleColorView(Context context) {
+        super(context);
+    }
+
+    public CycleColorView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray typedArray=context.obtainStyledAttributes(attrs, R.styleable.com_iori_custom_common_CycleColorView);
+        cycleColor =typedArray.getColor(R.styleable.com_iori_custom_common_CycleColorView_cycleColor,defaultColor);
+        frameColor=typedArray.getColor(R.styleable.com_iori_custom_common_CycleColorView_frameColor,defaultColor);
+        framePaddingPx=(int)typedArray.getDimension(R.styleable.com_iori_custom_common_CycleColorView_framePadding,0f);
+        frameWidthPx=(int)typedArray.getDimension(R.styleable.com_iori_custom_common_CycleColorView_frameWidth,0f);
+        typedArray.recycle();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width=calcSize(defaultSizePx,widthMeasureSpec);
+        int height=calcSize(defaultSizePx,heightMeasureSpec);
+
+        if(width < height){
+            height=width;
+        }else{
+            width=height;
+        }
+        setMeasuredDimension(width,height);
+    }
+
+    private int calcSize(int defaultSize,int measureSpec){
+        int calcSize=defaultSize;
+        int mode=MeasureSpec.getMode(measureSpec);
+        int size=MeasureSpec.getSize(measureSpec);
+        switch (mode){
+            case MeasureSpec.AT_MOST:
+            case MeasureSpec.EXACTLY:
+                calcSize=size;
+                break;
+        }
+        return calcSize;
+    }
+
+    public void setCycleColorAndReDraw(int color){
+        this.cycleColor =color;
+        invalidate();
+    }
+
+    public void setFrameColorAndReDraw(int color){
+        this.frameColor=color;
+        invalidate();
+    }
+
+    public void setFrameWidthPxAndReDraw(int frameWidthPx){
+        this.frameWidthPx=frameWidthPx;
+        invalidate();
+    }
+
+    public void setFramePaddingPxAndDraw(int framePaddingPx){
+        this.framePaddingPx=framePaddingPx;
+        invalidate();
+    }
+
+    private void drawCircle(Canvas canvas, int color, int frameWidth, int framePadding){
+        Point center=new Point();
+        int radius=getMeasuredWidth()/2-frameWidth-framePadding;
+        center.x=getLeft()+frameWidth+framePadding+radius;
+        center.y=getTop()+frameWidth+framePadding+radius;
+        Paint paint = new Paint();
+        paint.setColor(color);
+        canvas.drawCircle(center.x, center.y, radius, paint);
+    }
+
+    private void drawClickedRect(Canvas canvas,int color,int frameWidthPx) {
+        Paint paint = new Paint();
+        paint.setColor(color);
+        paint.setStrokeWidth(frameWidthPx);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(getLeft(), getTop(), getRight()-drawOffsetPx, getBottom()-drawOffsetPx, paint);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawCircle(canvas, cycleColor, frameWidthPx, framePaddingPx);
+        if(choose){
+            drawClickedRect(canvas,frameColor,frameWidthPx);
+        }
+    }
+
+    public void choose(boolean choose){
+        this.choose=choose;
+        invalidate();
+    }
+
+    public int getCycleColor() {
+        return cycleColor;
+    }
+
+    public int getFrameColor() {
+        return frameColor;
+    }
+
+    public int getFramePaddingPx() {
+        return framePaddingPx;
+    }
+
+    public int getFrameWidthPx() {
+        return frameWidthPx;
+    }
+}
