969d0ce558ce3b8efa3a487cbc26dcdeb21ef4ad
iori <iori71721@gmail.com>
2020-07-03 17:46:55 +0800

    version 1.9.0 1.螢幕截圖，可擷取包含SurfaceView

---
+++ common/src/main/java/com/iori/custom/common/image/Screenshoter.java
@@ -0,0 +1,89 @@
+package com.iori.custom.common.image;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
+import android.hardware.display.DisplayManager;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+
+import java.nio.ByteBuffer;
+
+import static android.content.Context.MEDIA_PROJECTION_SERVICE;
+
+/**
+ * useage:
+ * 1.{@link #startScreenShot(Activity, int)}
+ * 2.activity#onActivityResult() call {@link #fetchScreenShot(int, Intent)}
+ */
+public class Screenshoter {
+    private int width;
+    private int height;
+    private int dpi;
+    private MediaProjectionManager mediaProjectionManager;
+    private final ScreenShotBehavior screenShotBehavior;
+
+    public Screenshoter(ScreenShotBehavior screenShotBehavior) {
+        this.screenShotBehavior=screenShotBehavior;
+    }
+
+    public void startScreenShot(Activity activity,int requestCode) {
+        WindowManager windowManager = (WindowManager) activity.getSystemService(Context.WINDOW_SERVICE);
+        if (windowManager != null) {
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+            width = displayMetrics.widthPixels;
+            height = displayMetrics.heightPixels;
+            dpi = displayMetrics.densityDpi;
+        }
+
+        mediaProjectionManager = (MediaProjectionManager) activity.getSystemService(MEDIA_PROJECTION_SERVICE);
+        if (mediaProjectionManager != null) {
+            activity.startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(), requestCode);
+        }
+    }
+
+    public void fetchScreenShot(int resultCode, @Nullable Intent data){
+        MediaProjection mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data);
+        if (mediaProjection != null){
+            getBitmap(mediaProjection);
+        }
+    }
+
+    private void getBitmap(MediaProjection mediaProjection) {
+        final ImageReader imageReader = ImageReader.newInstance(width, height, PixelFormat.RGBA_8888, 1);
+        mediaProjection.createVirtualDisplay("screen_shot",
+                width, height, dpi, DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                imageReader.getSurface(), null, null);
+        imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                Image image = reader.acquireNextImage();
+                int width = image.getWidth();
+                int height = image.getHeight();
+                final Image.Plane[] planes = image.getPlanes();
+                final ByteBuffer buffer = planes[0].getBuffer();
+                int pixelStride = planes[0].getPixelStride();
+                int rowStride = planes[0].getRowStride();
+                int rowPadding = rowStride - pixelStride * width;
+                Bitmap bitmap = Bitmap.createBitmap(width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888);
+                bitmap.copyPixelsFromBuffer(buffer);
+                screenShotBehavior.screenShot(bitmap);
+                image.close();
+                imageReader.setOnImageAvailableListener(null,null);
+            }
+        }, null);
+    }
+
+    public static interface ScreenShotBehavior{
+        void screenShot(Bitmap bitmap);
+    }
+}
